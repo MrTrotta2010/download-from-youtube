d295644c88ea7b2cd1c28b574ea454497d8e6744
Thiago Silva <48692251+thiagodks@users.noreply.github.com>
2021-11-18 23:17:08 -0300

    Update in_out.py

--- in_out.py
+++ in_out.py
@@ -2,11 +2,11 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', required=True, type=str, help='Video url: -u https://youtube.com/asdasdasd')
-parser.add_argument('-o', required=True, default='pose', type=str, help='Output folder: -o videos')
-parser.add_argument('-w', required=True, default='pose', type=str, help='Website of origin: -w <youtube>')
+parser.add_argument('--url', required=True, type=str, help='Video url: -u https://youtube.com/asdasdasd')
+parser.add_argument('--output', required=True, default='pose', type=str, help='Output folder: -o videos')
+parser.add_argument('--website', required=True, default='pose', type=str, help='Website of origin: -w <youtube>')
 parser.add_argument('--playlist', action='store_true', help='If the specified url is a playlist')
 
 def parse_args():
-	args = vars(parser.parse_args())
-	return args['u'], args['o'], args['w'], args['playlist']
+	args = parser.parse_args()
+	return args.url, args.output, args.website, args.playlist
